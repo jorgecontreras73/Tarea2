2fba1dae6177a691528968aa5454473418f6950d
jorgecontreras73 <jorgecarlos73@gmail.com>
2021-04-03 16:42:21 -0300

    añadiendo

---
+++ js/inicio.js
@@ -0,0 +1,8 @@
+"use strict";
+var nombre;
+var apellido;
+var RUT;
+var correo;
+var telefono;
+var lenguaje;
+console.log("ALO?");
---
+++ ts/inicio.ts
@@ -0,0 +1,8 @@
+let nombre:string;
+let apellido:string;
+let RUT:string;
+let correo:string;
+let telefono:number;
+let lenguaje;
+
+console.log("ALO?")
